64897b5dd639e52a8e09965f099dba6d731f8dd2
Eric <equality509@gmail.com>
2023-01-22 18:53:10 -0500

    backend done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import router from "./controllers/student.js";
 
 // Get env variables
 dotenv.config();
@@ -18,10 +18,11 @@ app.use(express.json());
 
 // Routes and routers
 app.get("/", (req, res) => {
-    res.json({message: "it works!"})
+    res.json({message: "gotcha!"})
 })
 
-
+// Router
+app.use("/student", router);
 
 // Listener
 const PORT = process.env.PORT ?? 4001;
